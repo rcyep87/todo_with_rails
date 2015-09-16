0bcc9ec1f72dca356c6bfd76e1a7c5d4e69a3bd5
Ryan Yep <ryan.yep@gmail.com>
2015-09-16 15:03:54 -0500

    displays just a single todo as JSON

--- app/controllers/taskmanagers_controller.rb
+++ app/controllers/taskmanagers_controller.rb
@@ -1,6 +1,19 @@
 class TaskmanagersController < ApplicationController
+
   def index
     tasks = Taskmanager.all
     render json: tasks.to_json, status: 200
+  end
+
+  def show
+    if Taskmanager.exists?(params[:id])
+      task = Taskmanager.find(params[:id])
+      render json: task.to_json, status: 200
+    else
+      render json: { error_msg: "Record not found!", id: params[:id] }.to_json, status: 404
+    end
+  end
+
+  def new
   end
 end
